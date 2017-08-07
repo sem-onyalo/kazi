be952e5ed53d9dd36fa437eb657c8320e34d7579
oonyalo@gmail.com <oonyalo@gmail.com>
2017-08-07 11:56:56 -0400

    remove temp logging

--- api/routes/index.js
+++ api/routes/index.js
@@ -38,10 +38,7 @@ module.exports = function(app) {
 }
 
 async function authorizeRequest(req, res, next) {
-  console.log('***TEMP-LOG*** Request path: ' + req.path + ' method: ' + req.method);
   if (openPaths.indexOf(req.path) < 0 && openMethods.indexOf(req.method) < 0) {
-    console.log('***TEMP-LOG*** Request path: ' + req.path + ' method: ' + req.method + ' [CHECK_AUTH]');
-    console.log('***TEMP-LOG*** Request path: ' + req.path + ' method: ' + req.method, req.session);
     if (req.session && req.session.user) {
       let userRepository = DependencyFactory.resolve(Datasource.UserRepository);
       let user = await userRepository.getByUsername(req.session.user.Username);
