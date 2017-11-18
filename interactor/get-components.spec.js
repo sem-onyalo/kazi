6785a988ef549850674ce6877f91ff99772e8037
Sem Onyalo <oonyalo@gmail.com>
2017-11-18 12:02:55 -0500

    clean up get components interactor unit tests

--- interactor/get-components.spec.js
+++ interactor/get-components.spec.js
@@ -4,11 +4,11 @@ const chai = require('chai');
 const sinon = require('sinon');
 const assert = chai.assert;
 const expect = chai.expect;
-chai.use(require('chai-as-promised'));
 
 const Datasource = require('../datasource');
 const Entity = require('../entity');
 const GetComponents = require('./get-components');
+const GetComponentsRequest = require('./model/get-components-request');
 
 describe('GetComponents', () => {
   let componentRepository;
@@ -24,27 +24,41 @@ describe('GetComponents', () => {
       expect(getComponents.execute).to.be.a('function');
     });
 
-    it('should call ComponentRepository.get ot retrieve the components', async () => {
+    it('should call component repository\'s get method', async () => {
+      let request = new GetComponentsRequest();
+
       let getComponentsStub = sinon
         .stub(componentRepository, 'get');
 
-      await getComponents.execute();
-      getComponentsStub.restore();
+      await getComponents.execute(request);
+
+      sinon.assert.calledOnce(getComponentsStub);
+    });
+    
+    it('should call component repository\'s getRelationshipByDirectoryId method', async () => {
+      let request = new GetComponentsRequest(1);
+
+      let getComponentsStub = sinon
+        .stub(componentRepository, 'getRelationshipByDirectoryId');
+
+      await getComponents.execute(request);
 
       sinon.assert.calledOnce(getComponentsStub);
     });
 
     it('should return a collection of components', async () => {
+      let request = new GetComponentsRequest();
       let expectedComponents = [
         new Entity.Component(1, 'attendance', 'Attendance'),
         new Entity.Component(2, 'volunteer', 'Volunteer'),
         new Entity.Component(3, 'location', 'Location')
       ];
+
       let getComponentsStub = sinon
         .stub(componentRepository, 'get')
         .returns(Promise.resolve(expectedComponents));
 
-      let components = await getComponents.execute();
+      let components = await getComponents.execute(request);
 
       assert.strictEqual(components, expectedComponents, 'The returned components collection was not the expected value');
     });
