4ced8c9648e6bda86f06100849840e1ed7a3ab18
Sem Onyalo <oonyalo@gmail.com>
2017-11-18 16:21:55 -0500

    clean up update task interactor unit tests

--- interactor/update-task.spec.js
+++ interactor/update-task.spec.js
@@ -3,6 +3,7 @@
 const assert = require('chai').assert;
 const expect = require('chai').expect;
 const sinon = require('sinon');
+require('chai').use(require('chai-as-promised'));
 
 const Datasource = require('../datasource');
 const Entity = require('../entity');
@@ -24,47 +25,44 @@ describe('UpdateTask', () => {
     });
 
     it('should throw an exception if the task name is empty', () => {
-      let request = new UpdateTaskRequest(1, '');
-      let updateTaskFn = function() { updateTask.execute(request); };
-      expect(updateTaskFn).to.throw('The task name cannot be empty');
-
-      request = new UpdateTaskRequest(1, null);
-      updateTaskFn = function() { updateTask.execute(request); };
-      expect(updateTaskFn).to.throw('The task name cannot be empty');
-
-      request = new UpdateTaskRequest(1, undefined);
-      updateTaskFn = function() { updateTask.execute(request); };
-      expect(updateTaskFn).to.throw('The task name cannot be empty');
+      let promises = [];
+      let data = ['', undefined, null];
+      for (let i = 0; i < data.length; i++) {
+        let request = new UpdateTaskRequest(1, data[i]);
+        promises.push(assert.isRejected(updateTask.execute(request), 'The task name cannot be empty'));
+      }
+      
+      return Promise.all(promises);
     });
 
     it('should throw an exception if the task id does not exist', () => {
       let request = new UpdateTaskRequest(1, 'finish update task interactor');
+
       let getTaskByIdStub = sinon
         .stub(taskRepository, 'getById')
         .returns(null);
 
-      let updateTaskFn = function() { updateTask.execute(request); };
-
-      expect(updateTaskFn).to.throw('The specified task does not exist');
-
-      getTaskByIdStub.restore();
-
-      sinon.assert.calledOnce(getTaskByIdStub);
-      sinon.assert.calledWith(getTaskByIdStub, 1);
+      return assert.isRejected(updateTask.execute(request), 'The specified task does not exist')
+        .then(() => {
+          sinon.assert.calledOnce(getTaskByIdStub);
+          sinon.assert.calledWith(getTaskByIdStub, 1);
+        });
     });
 
-    it('should update the task', () => {
+    it('should update the task', async () => {
       let request = new UpdateTaskRequest(1, 'finish update task interactor');
       let existingTask = new Entity.Task(1, 'finish create task interactor', 1, 'inbox', 'Inbox');
       let expectedUpdatedTask = new Entity.Task(1, 'finish update task interactor', 1, 'inbox', 'Inbox');
+
       let getTaskByIdStub = sinon
         .stub(taskRepository, 'getById')
         .returns(existingTask);
+
       let updateTaskStub = sinon
         .stub(taskRepository, 'update')
         .returns(expectedUpdatedTask);
 
-      let task = updateTask.execute(request);
+      let task = await updateTask.execute(request);
 
       getTaskByIdStub.restore();
       updateTaskStub.restore();
