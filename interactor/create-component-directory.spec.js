65060f0f9e95184b3bcbfe7582309cb334a08f74
Sem Onyalo <oonyalo@gmail.com>
2017-11-18 07:25:18 -0500

    clean up create component directory interactor unit tests

--- interactor/create-component-directory.spec.js
+++ interactor/create-component-directory.spec.js
@@ -3,6 +3,7 @@
 const assert = require('chai').assert;
 const expect = require('chai').expect;
 const sinon = require('sinon');
+require('chai').use(require('chai-as-promised'));
 
 const CreateComponentDirectory = require('./create-component-directory');
 const CreateComponentDirectoryRequest = require('./model/create-component-directory-request');
@@ -23,30 +24,36 @@ describe('CreateComponentDirectory', () => {
       expect(createComponentDirectory.execute).to.be.a('function');
     });
 
-    it('should add the component to the directory', () => {
+    it('should add the component to the directory', async () => {
       let componentId = 1, directoryId = 2;
       let expectedComponent = new Entity.Component(componentId, 'event', 'Event');
       expectedComponent.DirectoryId = directoryId;
+
       let addComponentToDirectoryStub = sinon
         .stub(componentRepository, 'addToDirectory')
-        .returns(expectedComponent);
+        .returns(Promise.resolve(1));
+
+      let getComponentStub = sinon
+        .stub(componentRepository, 'getByComponentIdAndDirectoryId')
+        .returns(Promise.resolve(expectedComponent));
 
       let request = new CreateComponentDirectoryRequest(componentId, directoryId);
-      let component = createComponentDirectory.execute(request);
+      let component = await createComponentDirectory.execute(request);
 
       sinon.assert.calledWith(addComponentToDirectoryStub, componentId, directoryId);
+      sinon.assert.calledWith(getComponentStub, componentId, directoryId);
       assert.deepEqual(component, expectedComponent, 'The returned component was not the expected value');
     });
 
-    it('should throw an expection if there was an error adding the component to the directory or the component or directory does not exist', () => {
+    it('should throw an expection if there was an error adding the component to the directory or the component or directory does not exist', async () => {
       let componentId = 1, directoryId = 2;
+      
       let addComponentToDirectoryStub = sinon
         .stub(componentRepository, 'addToDirectory')
-        .returns(null);
+        .returns(0);
 
       let request = new CreateComponentDirectoryRequest(componentId, directoryId);
-      let createFn = function () { createComponentDirectory.execute(request); };
-      expect(createFn).to.throw('There was an error adding the component to the directory or the component or directory does not exist');
+      return assert.isRejected(createComponentDirectory.execute(request), 'There was an error adding the component to the directory or the component or directory does not exist');
     });
   });
 });
