346e5d27f377f4973684c4d42e6bc9a7a5111683
Sem Onyalo <oonyalo@gmail.com>
2017-11-18 11:50:20 -0500

    clean up create user interactor unit tests

--- interactor/create-user.spec.js
+++ interactor/create-user.spec.js
@@ -3,11 +3,13 @@
 const assert = require('chai').assert;
 const expect = require('chai').expect;
 const sinon = require('sinon');
+require('chai').use(require('chai-as-promised'));
 
 const CreateUser = require('./create-user');
 const CreateUserRequest = require('./model/create-user-request');
 const Datasource = require('../datasource');
 const Entity = require('../entity');
+const SecurityHelper = require('../util/security-helper');
 
 describe('CreateUser', () => {
   let request;
@@ -30,63 +32,58 @@ describe('CreateUser', () => {
     });
 
     it('should throw an exception if the first name is empty', () => {
-      request.FirstName = null;
-      let createUserFn = function () { createUser.execute(request); };
-      expect(createUserFn).to.throw('The first name cannot be empty');
+      let promises = [];
+      let data = ['', undefined, null];
+      for (let i = 0; i < data.length; i++) {
+        request.FirstName = data[i];
+        promises.push(assert.isRejected(createUser.execute(request), 'The first name cannot be empty'));
+      }
 
-      request.FirstName = undefined;
-      createUserFn = function () { createUser.execute(request); };
-      expect(createUserFn).to.throw('The first name cannot be empty');
-
-      request.FirstName = '';
-      createUserFn = function () { createUser.execute(request); };
-      expect(createUserFn).to.throw('The first name cannot be empty');
+      return Promise.all(promises);
     });
 
     it('should throw an exception if the last name is empty', () => {
-      request.LastName = null;
-      let createUserFn = function () { createUser.execute(request); };
-      expect(createUserFn).to.throw('The last name cannot be empty');
+      let promises = [];
+      let data = ['', undefined, null];
+      for (let i = 0; i < data.length; i++) {
+        request.LastName = data[i];
+        promises.push(assert.isRejected(createUser.execute(request), 'The last name cannot be empty'));
+      }
 
-      request.LastName = undefined;
-      createUserFn = function () { createUser.execute(request); };
-      expect(createUserFn).to.throw('The last name cannot be empty');
-
-      request.LastName = '';
-      createUserFn = function () { createUser.execute(request); };
-      expect(createUserFn).to.throw('The last name cannot be empty');
+      return Promise.all(promises);
     });
 
     it('should throw an exception if the username is empty', () => {
-      request.Username = null;
-      let createUserFn = function () { createUser.execute(request); };
-      expect(createUserFn).to.throw('The username cannot be empty');
+      let promises = [];
+      let data = ['', undefined, null];
+      for (let i = 0; i < data.length; i++) {
+        request.Username = data[i];
+        promises.push(assert.isRejected(createUser.execute(request), 'The username cannot be empty'));
+      }
 
-      request.Username = undefined;
-      createUserFn = function () { createUser.execute(request); };
-      expect(createUserFn).to.throw('The username cannot be empty');
-
-      request.Username = '';
-      createUserFn = function () { createUser.execute(request); };
-      expect(createUserFn).to.throw('The username cannot be empty');
+      return Promise.all(promises);
     });
 
     it('should throw an exception if password is not empty and does not meet strength requirements (at least 8 or more, one upper, one lower, one numeric, and one special character)', () => {
+      let promises = [];
       let data = ['123456789', 'abcdefghi', 'ABCDEFGHI', '!@#$%^&*()', 'P@w1'];
       for (var i = 0; i < data.length; i++) {
         request.Password = data[i];
-        let createUserFn = function () { createUser.execute(request); };
-        expect(createUserFn).to.throw('The password must be a minimum of 8 characters and include at least one upper case, one lower case, one numeric, and one special character');
+        promises.push(assert.isRejected(createUser.execute(request), 'The password must be a minimum of 8 characters and include at least one upper case, one lower case, one numeric, and one special character'));
       }
+
+      return Promise.all(promises);
     });
 
     it('should throw an exception if user role is not expected value', () => {
+      let promises = [];
       let data = [0, 1, 4];
       for (var i = 0; i < data.length; i++) {
         request.UserRole = data[i];
-        let createUserFn = function () { createUser.execute(request); };
-        expect(createUserFn).to.throw('The user role is invalid');
+        promises.push(assert.isRejected(createUser.execute(request), 'The user role is invalid'));
       }
+
+      return Promise.all(promises);
     });
 
     it('should throw an exception if the association does not exist', () => {
@@ -94,16 +91,14 @@ describe('CreateUser', () => {
         .stub(associationRepository, 'getById')
         .returns(null);
 
-      let createUserFn = function () { createUser.execute(request); };
-      expect(createUserFn).to.throw('The specified association does not exist');
-
-      getAssociationByIdStub.restore();
-
-      sinon.assert.calledOnce(getAssociationByIdStub);
-      sinon.assert.calledWith(getAssociationByIdStub, request.AssociationId);
+      return assert.isRejected(createUser.execute(request), 'The specified association does not exist')
+        .then(() => {
+          sinon.assert.calledOnce(getAssociationByIdStub);
+          sinon.assert.calledWith(getAssociationByIdStub, request.AssociationId);
+        });
     });
 
-    it('should create the user', () => {
+    it('should create the user', async () => {
       let expectedUser = new Entity.User(1, 'John', 'Doe', 'john.doe', 'Password1!', '', Entity.UserRole.USER);
       let expectedNewUser = new Entity.User(0, 'John', 'Doe', 'john.doe', 'Password1!', '', Entity.UserRole.USER);
 
@@ -111,14 +106,25 @@ describe('CreateUser', () => {
         .stub(associationRepository, 'getById')
         .returns(new Entity.Association(1, 'my-association', 'My Association', 'Company'));
 
+      let getUserStub = sinon
+        .stub(userRepository, 'getByUsername')
+        .returns(null);
+
       let createUserStub = sinon
         .stub(userRepository, 'create')
         .returns(expectedUser);
 
-      let actualUser = createUser.execute(request);
+      let addAssociationUserStub = sinon
+        .stub(associationRepository, 'addUser')
+        .returns(1);
 
-      getAssociationByIdStub.restore();
-      createUserStub.restore();
+      let hashPasswordStub = sinon
+        .stub(SecurityHelper, 'hashPassword')
+        .returns('Password1!');
+
+      let actualUser = await createUser.execute(request);
+
+      hashPasswordStub.restore();
 
       sinon.assert.calledOnce(getAssociationByIdStub);
       sinon.assert.calledWith(getAssociationByIdStub, request.AssociationId);
@@ -129,26 +135,38 @@ describe('CreateUser', () => {
       assert.deepEqual(actualUser, expectedUser, 'Returned user object was not expected value');
     });
 
-    it('should create the user with an auto-generated auth token if the password is empty', () => {
+    it('should create the user with an auto-generated auth token if the password is empty', async () => {
       let expectedAuthTokenLength = 64;
 
       let getAssociationByIdStub = sinon
         .stub(associationRepository, 'getById')
         .returns(new Entity.Association(1, 'my-association', 'My Association', 'Company'));
 
+      let getUserStub = sinon
+        .stub(userRepository, 'getByUsername')
+        .returns(null);
+
       let createUserStub = sinon
         .stub(userRepository, 'create')
         .returnsArg(0);
+        
+      let addAssociationUserStub = sinon
+        .stub(associationRepository, 'addUser')
+        .returns(1);
+
+      let hashPasswordStub = sinon
+        .stub(SecurityHelper, 'hashPassword')
+        .returns('Password1!');
 
       let data = [null, undefined, ''];
       for (let i = 0; i < data.length; i++) {
         request.Password = data[i];
-        let actualUser = createUser.execute(request);
+        let actualUser = await createUser.execute(request);
 
         assert.strictEqual(actualUser.AuthToken.length, expectedAuthTokenLength, 'Auth token should have been generated if password is empty');
       }
-
-      createUserStub.restore();
+      
+      hashPasswordStub.restore();
     });
   });
 });
